ea89e0f260e64a51ff44eeb70def9746b9c4df6a
Michal Niegrzybowski <micnie@jetshop.se>
2018-08-19 21:39:18 +0200

    Add project files.

---
+++ PerformanceTestOfListInsertion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PerformanceTestOfListInsertion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
